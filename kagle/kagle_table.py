ead2a1a9bbc86aa8ac63cec231cc604847ea1efa
xiexionghang <xiexionghang@baidu.com>
2020-03-05 19:17:57 +0800

    commit kagle for paddle

--- kagle/kagle_table.py
+++ kagle/kagle_table.py
@@ -1,12 +1,24 @@
+"""
+Construct ParamTable Meta 
+"""
 import copy
 import yaml
-from abc import ABCMeta, abstractmethod
 
-class TableMeta:
+class TableMeta(object):
+    """
+    Simple ParamTable Meta, Contain table_id
+    """
     TableId = 1
     
     @staticmethod
     def alloc_new_table(table_id):
+        """
+        create table with table_id
+        Args:
+            table_id(int) 
+        Return:
+            table(TableMeta)  :  a TableMeta instance with table_id
+        """
         if table_id < 0:
             table_id = TableMeta.TableId
         if table_id >= TableMeta.TableId:
@@ -15,5 +27,5 @@ class TableMeta:
         return table
 
     def __init__(self, table_id):
+        """ """
         self._table_id = table_id
-        pass
