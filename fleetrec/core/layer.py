b8d014575f1e101e3b6f25180a9cd2f41908cbea
tangwei <tangwei12@baidu.com>
2020-04-14 18:47:09 +0800

    structure rebuild

---
+++ fleetrec/core/layer.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import abc
+import copy
+import yaml
+import paddle.fluid as fluid
+from ..utils import table as table
+from paddle.fluid.incubate.fleet.parameter_server.pslib import fleet
+
+
+class Layer(object):
+    """R
+    """
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, config):
+        """R
+        """
+        pass
+
+    def generate(self, mode, param):
+        """R
+        """
+        if mode == 'fluid':
+            return self.generate_fluid(param)
+        elif mode == 'tensorflow':
+            return self.generate_tensorflow(param)
+        print('unsupport this mode: ' + mode)
+        return None, None
+
+    @abc.abstractmethod
+    def generate_fluid(self, param):
+        """R
+        """
+        pass
+
+    def generate_tensorflow(self, param):
+        """ Not implement currently
+        """
+        pass
---
+++ fleetrec/core/model.py
@@ -0,0 +1,40 @@
+import abc
+
+
+class Model(object):
+    """R
+    """
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, config):
+        """R
+        """
+        self._cost = None
+        self._metrics = {}
+        self._data_var = []
+        self._fetch_interval = 20
+
+    def get_inputs(self):
+        return self._data_var
+
+    def get_cost_op(self):
+        """R
+        """
+        return self._cost
+
+    def get_metrics(self):
+        """R
+        """
+        return self._metrics
+
+    def get_fetch_period(self):
+        return self._fetch_interval
+
+    @abc.abstractmethod
+    def train_net(self):
+        """R
+        """
+        pass
+
+    def infer_net(self):
+        pass
