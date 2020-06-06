07f9c21fe3c81570f74877b6170492a1e4d377e0
malin10 <malin10@baidu.com>
2020-06-06 08:26:14 +0800

    change directory

--- models/demo/movie_recommand/data_prepare.sh
+++ models/demo/movie_recommand/data_prepare.sh
@@ -10,8 +10,8 @@ mkdir test/
 
 python process_ml_1m.py process_raw ./ml-1m/train.dat | sort -t $'\t' -k 9 -n > log.data.train
 python process_ml_1m.py process_raw ./ml-1m/test.dat | sort -t $'\t' -k 9 -n > log.data.test
-python process_ml_1m.py hash log.data.train > ./train/log.data.hash
-python process_ml_1m.py hash log.data.test > ./test/log.data.hash
+python process_ml_1m.py hash log.data.train > ./train/data.txt
+python process_ml_1m.py hash log.data.test > ./test/data.txt
 
 rm log.data.train
 rm log.data.test
