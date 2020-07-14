ae2786231c33e7e931a478ab93dbdb8030a71d1a
yinhaofeng <1841837261@qq.com>
2020-07-14 08:41:07 +0000

    add some code test=develop

---
+++ models/match/match-pyramid/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "..............test................."
+python -m paddlerec.run -m ./config_test.yaml >result1.txt
+grep -A1 "prediction" ./result1.txt >./result.txt
+rm -f result1.txt
+python eval.py
