3ac2d65635783802fc543b892f2f9c8b24eaf95e
yinhaofeng <1841837261@qq.com>
2020-07-16 14:11:30 +0000

    add code

---
+++ models/match/match-pyramid/data_process.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "...........load  data................."
+wget --no-check-certificate 'https://paddlerec.bj.bcebos.com/match_pyramid/match_pyramid_data.tar.gz'
+mv ./match_pyramid_data.tar.gz ./data
+rm -rf ./data/relation.test.fold1.txt ./data/realtion.train.fold1.txt
+tar -xvf ./data/match_pyramid_data.tar.gz
+echo "...........data process..............."
+python ./data/process.py
