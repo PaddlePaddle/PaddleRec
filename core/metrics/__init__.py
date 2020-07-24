c6e9c2f501afa25561f143b7bf86869c3cb6cad6
malin10 <malin10@baidu.com>
2020-07-24 15:14:14 +0800

    fix python3 import err

--- core/metrics/__init__.py
+++ core/metrics/__init__.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from precision import Precision
+from .precision import Precision
 
 __all__ = ['Precision']
