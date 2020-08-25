2ff350a9a8efac3b946a48cac890b29a8f2e0cc9
yinhaofeng <1841837261@qq.com>
2020-08-25 02:16:43 +0000

    change

--- tools/cal_pos_neg.py
+++ tools/cal_pos_neg.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#!/usr/bin/python  
+#!/usr/bin/python   
 #-*- coding:utf-8 -*-   
 """
 docstring
