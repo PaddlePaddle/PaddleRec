602dea30d5e85b219b6363b2a567d66ca8134d87
chengmo <chengmo@baidu.com>
2020-09-09 20:41:01 +0800

    fix

--- models/demo/movie_recommand/online_rank.sh
+++ models/demo/movie_recommand/online_rank.sh
@@ -5,5 +5,5 @@ python process_ml_1m.py data_rank > online_user/test/data.txt
 cd ..
 echo "Rank online test ..."
 echo "Model config at models/demo/movie_recommand/rank/config_online_test.yaml"
-python -m paddlerec.run -m ./config_test_online.yaml
-python parse.py rank_online rank/infer_result
+python -m paddlerec.run -m ./rank/config_test_online.yaml
+python parse.py rank_online ./rank/infer_result
