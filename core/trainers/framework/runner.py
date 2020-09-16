fd2ca3071fcbdf88811b55d7b7545d98162accf4
liuyuhui <liuyuhui@baidu.com>
2020-09-16 14:27:24 +0800

    fix bug

--- core/trainers/framework/runner.py
+++ core/trainers/framework/runner.py
@@ -213,16 +213,12 @@ class RunnerBase(object):
                             if context["fleet_mode"].upper() == "PS":
                                 train_prog = context["model"][model_dict[
                                     "name"]]["main_program"]
-                                print("condition 1 of bath id:{}".format(
-                                    batch_id))
                             else:
                                 train_prog = context["model"][model_dict[
                                     "name"]]["default_main_program"]
-                                print("condition 2")
                         else:
                             train_prog = context["model"][model_dict["name"]][
                                 "default_main_program"]
-                            print("condition 3")
                         startup_prog = context["model"][model_dict["name"]][
                             "startup_program"]
                         with fluid.program_guard(train_prog, startup_prog):
