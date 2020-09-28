be050841b773ef53702239d2f9aa671a158c4207
Chengmo <cmchengmo@163.com>
2020-09-28 19:41:07 +0800

    Six PR for new paddlerec  version (#240)
    * logistic_regression
    
    * adaptation windows
    
    Co-authored-by: yinhaofeng <1841837261@qq.com>
    Co-authored-by: yinhaofeng <66763551+yinhaofeng@users.noreply.github.com>
    Co-authored-by: wuzhihua <35824027+fuyinno4@users.noreply.github.com>
    Co-authored-by: tangwei12 <tangwei12@baidu.com>

--- models/match/match-pyramid/run.sh
+++ models/match/match-pyramid/run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 echo "................run................."
-python -m paddlerec.run -m ./config.yaml &>result1.txt
-grep -i "prediction" ./result1.txt >./result2.txt
-sed '$d' result2.txt >result.txt
-rm -f result2.txt
-rm -f result1.txt
+python -m paddlerec.run -m ./config.yaml &>result.txt
 python eval.py
--- models/rank/wide_deep/data/create_data.sh
+++ models/rank/wide_deep/data/create_data.sh
@@ -1,11 +1,11 @@
 mkdir train_data
 mkdir test_data
-train_path="adult.data"
-test_path="adult.test"
+train_path="data/adult.data" 
+test_path="data/adult.test" 
 train_data_path="./train_data/train_data.csv"
 test_data_path="./test_data/test_data.csv"
 
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 wget -P data/ https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data
 wget -P data/ https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test
