ec78fa39b275cce3e332c2845a6467d8b9e1aabf
MrChengmo <cmchengmo@163.com>
2020-11-13 16:45:04 +0800

    fix

--- models/rank/ctr_dnn_criteo/before_hook.sh
+++ models/rank/ctr_dnn_criteo/before_hook.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # download paddlerec
-hadoop fs -D fs.default.name=afs://yinglong.afs.baidu.com:9902 -D hadoop.job.ugi=paddle,paddle -get /user/paddle/chengmo/paddlerec/whl/ps_benchmark/PaddleRec.tar.gz ./
-tar -xf PaddleRec.tar.gz
 
 # install paddlerec
 cd PaddleRec
@@ -12,7 +10,6 @@ pip uninstall -y paddlepaddle
 pip uninstall -y paddlepaddle-gpu
 
 # downlaod paddlepaddle
-hadoop fs -D hadoop.job.ugi=paddle,paddle -D fs.default.name=afs://yinglong.afs.baidu.com:9902 -get /user/paddle/chengmo/whl/heter_ps_1029/paddlepaddle-0.0.0-cp27-cp27mu-linux_x86_64.whl ./
 
 # install paddlepaddle
 pip install paddlepaddle-0.0.0-cp27-cp27mu-linux_x86_64.whl --index-url=http://pip.baidu.com/pypi/simple --trusted-host pip.baidu.com
