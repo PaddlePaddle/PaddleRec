870d37692485f3edba531a6204d3aa1fef0c70c1
wangjiawei04 <wangjiawei04@baidu.com>
2020-12-14 03:28:36 +0000

    fix Doc

--- tools/recserving/movie_recommender/get_data.sh
+++ tools/recserving/movie_recommender/get_data.sh
@@ -2,7 +2,7 @@ wget https://paddlerec.bj.bcebos.com/aistudio/aistudio_paddlerec_rank.tar.gz --n
 tar xf aistudio_paddlerec_rank.tar.gz
 wget https://paddlerec.bj.bcebos.com/aistudio/user_vector.tar.gz --no-check-certificate
 tar xf user_vector.tar.gz
-wget https://paddlerec.bj.bcebos.com/aistudio/recall.dat --no-check-certificate
+wget https://paddlerec.bj.bcebos.com/aistudio/movie_vectors.txt --no-check-certificate
 wget https://paddlerec.bj.bcebos.com/aistudio/users.dat --no-check-certificate
 wget https://paddlerec.bj.bcebos.com/aistudio/movies.dat --no-check-certificate
 python3 to_redis.py
