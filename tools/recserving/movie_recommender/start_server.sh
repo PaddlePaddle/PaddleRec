61a04ce1f0af39ce08f2eb613759dd636221b93f
wangjiawei04 <wangjiawei04@baidu.com>
2020-12-14 03:22:18 +0000

    fix model

--- tools/recserving/movie_recommender/start_server.sh
+++ tools/recserving/movie_recommender/start_server.sh
@@ -2,6 +2,6 @@ cd proto && python run_codegen.py && cd ..
 export PYTHONPATH=$PYTHONPATH:$PWD/proto
 python3 um.py &
 python3 cm.py &
-python3 new_recall.py &
+python3 recall.py &
 python3 rank.py &
 python3 as.py &
