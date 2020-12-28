d795db70cdd21df9e106180f52d6694a982086ed
frankwhzhang <frankwhzhang@126.com>
2020-12-28 14:31:50 +0000

    fix wide_deep

--- models/recall/word2vec/utils_dygraph.py
+++ models/recall/word2vec/utils_dygraph.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ...utils import envs
+from paddlerec.core.utils import envs
 import os
 import copy
 import subprocess
