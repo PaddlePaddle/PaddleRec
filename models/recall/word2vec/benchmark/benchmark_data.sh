d48bfcb50142b5059ae8f4ea99e25b5337b606c1
MrChengmo <cmchengmo@163.com>
2021-01-23 20:02:40 +0800

    update benchmark

---
+++ models/recall/word2vec/benchmark/benchmark_data.sh
@@ -0,0 +1,5 @@
+echo "Begin DownLoad One Billion Data"
+wget --no-check-certificate https://paddlerec.bj.bcebos.com/benchmark/word2vec_benchmark_data.tar.gz
+echo "Begin Unzip One Billion Data"
+tar -xf word2vec_benchmark_data.tar.gz
+echo "Get One Billion Data Success"
