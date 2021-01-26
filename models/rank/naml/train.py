056acb2074a165d6d4544cf3a9c692c394880164
seemingwang <zsasuke@qq.com>
2021-01-26 22:19:33 +0800

    save file

--- models/rank/naml/train.py
+++ models/rank/naml/train.py
@@ -36,6 +36,7 @@ from NAMLDataReader import RecDataset
 from paddle.io import DistributedBatchSampler, DataLoader
 import argparse
 import net
+import paddle.fluid as fluid
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -70,8 +71,16 @@ def create_loss(raw_predict_2d, label):
 
 
 def create_model(config):
-
-    return net.NAMLLayer(config)
+    article_content_size = config.get("hyper_parameters.article_content_size")
+    article_title_size = config.get("hyper_parameters.article_title_size")
+    browse_size = config.get("hyper_parameters.browse_size")
+    neg_condidate_sample_size = config.get("hyper_parameters.neg_condidate_sample_size")
+    word_dimension = config.get("hyper_parameters.word_dimension")
+    category_size = config.get("hyper_parameters.category_size")
+    sub_category_size = config.get("hyper_parameters.sub_category_size")
+    cate_dimension = config.get("hyper_parameters.category_dimension")
+    word_dict_size = config.get("hyper_parameters.word_dict_size")
+    return net.NAMLLayer(article_content_size, article_title_size, browse_size, neg_condidate_sample_size, word_dimension, category_size, sub_category_size, cate_dimension, word_dict_size)
 
 
 def create_data_loader(dataset, place, config):
@@ -106,7 +115,7 @@ def main(args):
         load_model(model_init_path, dnn_model)
 
     optimizer = paddle.optimizer.Adam(parameters=dnn_model.parameters())
-
+    #optimizer = paddle.optimizer.SGD(parameters=dnn_model.parameters())
     file_list = [
         os.path.join(train_data_dir, x) for x in os.listdir(train_data_dir)
     ]
@@ -118,7 +127,8 @@ def main(args):
 
     for epoch_id in range(last_epoch_id + 1, epochs):
         dnn_model.train()
-        auc_metric = paddle.metric.Auc("ROC")
+        #auc_metric = paddle.metric.Auc("ROC")
+        metric = paddle.metric.Accuracy()
         epoch_begin = time.time()
         interval_begin = time.time()
         train_reader_cost = 0.0
@@ -132,75 +142,48 @@ def main(args):
             train_start = time.time()
             batch_size = len(batch[0])
 
-            label, sparse_tensor, dense_tensor = create_feeds(batch,
+            labels, sparse_tensor, dense_tensor = create_feeds(batch,
                                                               dense_input_dim)
 
-            labels = paddle.cast(label, "float32")
-            #print(labels)
-            #sampe_size = config.get("hyper_parameters.neg_condidate_sample_size") + 1
-            #label_mat = paddle.reshape(label,[-1,1,sampe_size])
-            cate_sample, cate_visit, sub_cate_sample, sub_cate_visit, title_sample, title_visit, content_sample, content_visit = sparse_tensor[:]
-            # cate_sample = paddle.reshape(cate_sample,[-1,sampe_size,1])
-            # cate_sample = paddle.matmul(label_mat, cate_sample)
-            # cate_sample = paddle.reshape(cate_sample,[-1])
-            # sub_cate_sample = paddle.reshape(sub_cate_sample,[-1,sampe_size,1])
-            # [b,1,1]
-            # sub_cate_sample = paddle.matmul(label_mat, sub_cate_sample)
-            # sub_cate_sample = paddle.reshape(sub_cate_sample, [-1])
-            raw_pred = dnn_model(cate_sample, cate_visit, sub_cate_sample, sub_cate_visit, title_sample, title_visit, content_sample, content_visit)
-            loss = paddle.nn.functional.cross_entropy(input = raw_pred, label = labels, soft_label=True)
+            raw = dnn_model(sparse_tensor, None)
+
+            loss = paddle.nn.functional.cross_entropy(input = raw, label = paddle.cast(labels, "float32"), soft_label=True)
+            correct = metric.compute(raw, labels)
+            metric.update(correct)
             loss = paddle.mean(loss)
             loss.backward()
             optimizer.step()
             train_run_cost += time.time() - train_start
             total_samples += batch_size
-        #     predict_2d = paddle.concat(x=[1 - raw_pred, raw_pred], axis=1)
-#            auc_metric.update(preds=raw_pred.numpy(), labels=label.numpy())
+            label = fluid.layers.assign(np.array([3, 3], dtype="int32"))
+            limit = fluid.layers.assign(np.array([3, 2], dtype="int32"))
+            #predict_2d = paddle.concat(x=[1 - raw_pred, raw_pred], axis=1)
+            #auc_metric.update(preds=predict_2d.numpy(), labels=labels.numpy())
 
             if batch_id % print_interval == 0:
-                print(raw_pred)
-                print(label)
-                # logger.info(
-                #     "epoch: {}, batch_id: {}, auc: {:.6f}, avg_reader_cost: {:.5f} sec, avg_batch_cost: {:.5f} sec, avg_samples: {:.5f}, ips: {:.5f} images/sec".
-                #     format(epoch_id, batch_id,
-                #            auc_metric.accumulate(), train_reader_cost /
-                #            print_interval, (train_reader_cost + train_run_cost
-                #                             ) / print_interval, total_samples /
-                #            print_interval, total_samples / (train_reader_cost +
-                #                                             train_run_cost)))
-                # train_reader_cost = 0.0
-                # train_run_cost = 0.0
-                # total_samples = 0
+                print(raw)
+                print(labels)
                 print(loss)
+                logger.info(
+                    "epoch: {}, batch_id: {}, auc: {:.6f}, avg_reader_cost: {:.5f} sec, avg_batch_cost: {:.5f} sec, avg_samples: {:.5f}, ips: {:.5f} images/sec".
+                    format(epoch_id, batch_id,
+                           metric.accumulate(), train_reader_cost /
+                           print_interval, (train_reader_cost + train_run_cost
+                                            ) / print_interval, total_samples /
+                           print_interval, total_samples / (train_reader_cost +
+                                                            train_run_cost)))
+                train_reader_cost = 0.0
+                train_run_cost = 0.0
+                total_samples = 0
             reader_start = time.time()
 
-        # logger.info("epoch: {} done, auc: {:.6f}, : epoch time{:.2f} s".format(
-        #     epoch_id, auc_metric.accumulate(), time.time() - epoch_begin))
+        logger.info("epoch: {} done, auc: {:.6f}, : epoch time{:.2f} s".format(
+            epoch_id, metric.accumulate(), time.time() - epoch_begin))
         #
         # save_model(
         #     dnn_model, optimizer, model_save_path, epoch_id, prefix='rec')
 
 
 if __name__ == '__main__':
-    from paddle.nn import Conv1D
-    import numpy as np
-
-    x = np.array([[[4, 8, 1, 9],
-                   [7, 2, 0, 9],
-                   [6, 9, 2, 6]]]).astype(np.float32)
-    w = np.array(
-        [[[9, 3, 4],
-          [0, 0, 7],
-          [2, 5, 6]],
-         [[0, 3, 4],
-          [2, 9, 7],
-          [5, 6, 8]]]).astype(np.float32)
-    x_t = paddle.to_tensor(x)
-    conv = Conv1D(3, 2, 3, padding = "same")
-    conv.weight.set_value(w)
-    y_t = conv(x_t)
-    print(y_t)
-    # [[[133. 238.]
-    #   [160. 211.]]]
     args = parse_args()
     main(args)
