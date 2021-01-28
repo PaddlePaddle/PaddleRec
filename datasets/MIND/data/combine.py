ece088c5a48491750dd738a6d8b85ece2ac72a12
seemingwang <zsasuke@qq.com>
2021-01-28 15:11:31 +0800

    bigdata shell change

--- datasets/MIND/data/combine.py
+++ datasets/MIND/data/combine.py
@@ -17,13 +17,6 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     count = 0
     cate_dict = {"<unk>": 0}
