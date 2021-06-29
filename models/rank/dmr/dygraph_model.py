a0990c0fbe53d89d0e31653f5fbb8765f696a43f
root <1841837261@qq.com>
2021-06-29 13:37:25 +0000

    dmr model

---
+++ models/rank/dmr/dygraph_model.py
@@ -0,0 +1,105 @@
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import paddle
+import paddle.nn as nn
+import paddle.nn.functional as F
+import math
+
+import net
+
+
+class DygraphModel():
+    # define model
+    def create_model(self, config):
+        user_size = config.get("hyper_parameters.user_size")
+        cms_segid_size = config.get("hyper_parameters.cms_segid_size")
+        cms_group_id_size = config.get("hyper_parameters.cms_group_id_size")
+        final_gender_code_size = config.get(
+            "hyper_parameters.final_gender_code_size")
+        age_level_size = config.get("hyper_parameters.age_level_size")
+        pvalue_level_size = config.get("hyper_parameters.pvalue_level_size")
+        shopping_level_size = config.get(
+            "hyper_parameters.shopping_level_size")
+        occupation_size = config.get("hyper_parameters.occupation_size")
+        new_user_class_level_size = config.get(
+            "hyper_parameters.new_user_class_level_size")
+        adgroup_id_size = config.get("hyper_parameters.adgroup_id_size")
+        cate_size = config.get("hyper_parameters.cate_size")
+        campaign_id_size = config.get("hyper_parameters.campaign_id_size")
+        customer_size = config.get("hyper_parameters.customer_size")
+        brand_size = config.get("hyper_parameters.brand_size")
+        btag_size = config.get("hyper_parameters.btag_size")
+        pid_size = config.get("hyper_parameters.pid_size")
+        main_embedding_size = config.get(
+            "hyper_parameters.main_embedding_size")
+        other_embedding_size = config.get(
+            "hyper_parameters.other_embedding_size")
+
+        dmr_model = net.DMRLayer(
+            user_size, cms_segid_size, cms_group_id_size,
+            final_gender_code_size, age_level_size, pvalue_level_size,
+            shopping_level_size, occupation_size, new_user_class_level_size,
+            adgroup_id_size, cate_size, campaign_id_size, customer_size,
+            brand_size, btag_size, pid_size, main_embedding_size,
+            other_embedding_size)
+
+        return dmr_model
+
+    # define feeds which convert numpy of batch data to paddle.tensor
+    def create_feeds(self, batch_data, config):
+        b = batch_data[0]
+        sparse_tensor = b.astype('int64')
+        dense_tensor = paddle.to_tensor(b[:, 264].numpy().astype('float32')
+                                        .reshape(-1, 1))
+        label = sparse_tensor[:, -1].reshape([-1, 1])
+        return label, [sparse_tensor, dense_tensor]
+
+    # define optimizer
+    def create_optimizer(self, dy_model, config):
+        lr = config.get("hyper_parameters.optimizer.learning_rate", 0.001)
+        optimizer = paddle.optimizer.Adam(
+            learning_rate=lr, parameters=dy_model.parameters())
+        return optimizer
+
+    # define metrics such as auc/acc
+    # multi-task need to define multi metric
+    def create_metrics(self):
+        metrics_list_name = ["auc"]
+        auc_metric = paddle.metric.Auc("ROC")
+        metrics_list = [auc_metric]
+        return metrics_list, metrics_list_name
+
+    # construct train forward phase
+    def train_forward(self, dy_model, metrics_list, batch_data, config):
+        label, input_tensor = self.create_feeds(batch_data, config)
+
+        pred, loss = dy_model(input_tensor, False)
+        # update metrics
+        predict_2d = paddle.concat(x=[1 - pred, pred], axis=1)
+        metrics_list[0].update(preds=predict_2d.numpy(), labels=label.numpy())
+
+        print_dict = {'loss': loss}
+        # print_dict = None
+        return loss, metrics_list, print_dict
+
+    def infer_forward(self, dy_model, metrics_list, batch_data, config):
+        label, input_tensor = self.create_feeds(batch_data, config)
+
+        pred, loss = dy_model(input_tensor, True)
+        # update metrics
+        predict_2d = paddle.concat(x=[1 - pred, pred], axis=1)
+        metrics_list[0].update(preds=predict_2d.numpy(), labels=label.numpy())
+
+        return metrics_list, None
---
+++ models/rank/dmr/static_model.py
@@ -0,0 +1,110 @@
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import math
+import paddle
+
+from net import DMRLayer
+
+
+class StaticModel():
+    def __init__(self, config):
+        self.cost = None
+        self.config = config
+        self._init_hyper_parameters()
+
+    def _init_hyper_parameters(self):
+        self.user_size = self.config.get("hyper_parameters.user_size")
+        self.cms_segid_size = self.config.get(
+            "hyper_parameters.cms_segid_size")
+        self.cms_group_id_size = self.config.get(
+            "hyper_parameters.cms_group_id_size")
+        self.final_gender_code_size = self.config.get(
+            "hyper_parameters.final_gender_code_size")
+        self.age_level_size = self.config.get(
+            "hyper_parameters.age_level_size")
+        self.pvalue_level_size = self.config.get(
+            "hyper_parameters.pvalue_level_size")
+        self.shopping_level_size = self.config.get(
+            "hyper_parameters.shopping_level_size")
+        self.occupation_size = self.config.get(
+            "hyper_parameters.occupation_size")
+        self.new_user_class_level_size = self.config.get(
+            "hyper_parameters.new_user_class_level_size")
+        self.adgroup_id_size = self.config.get(
+            "hyper_parameters.adgroup_id_size")
+        self.cate_size = self.config.get("hyper_parameters.cate_size")
+        self.campaign_id_size = self.config.get(
+            "hyper_parameters.campaign_id_size")
+        self.customer_size = self.config.get("hyper_parameters.customer_size")
+        self.brand_size = self.config.get("hyper_parameters.brand_size")
+        self.btag_size = self.config.get("hyper_parameters.btag_size")
+        self.pid_size = self.config.get("hyper_parameters.pid_size")
+        self.main_embedding_size = self.config.get(
+            "hyper_parameters.main_embedding_size")
+        self.other_embedding_size = self.config.get(
+            "hyper_parameters.other_embedding_size")
+
+        self.learning_rate = self.config.get(
+            "hyper_parameters.optimizer.learning_rate", 0.008)
+
+    def create_feeds(self, is_infer=False):
+        input = paddle.static.data(
+            name="sparse_tensor", shape=[None, 267], dtype="float32")
+        feed_list = [input]
+        return feed_list
+
+    def net(self, input, is_infer=False):
+        sparse = paddle.cast(input[0], "int64")
+        price = paddle.slice(input[0], [1], [264], [265])
+        label = paddle.slice(input[0], [1], [266], [267])
+        label = paddle.cast(label, "int64")
+        inputs = [sparse, price]
+
+        DMR_model = DMRLayer(
+            self.user_size, self.cms_segid_size, self.cms_group_id_size,
+            self.final_gender_code_size, self.age_level_size,
+            self.pvalue_level_size, self.shopping_level_size,
+            self.occupation_size, self.new_user_class_level_size,
+            self.adgroup_id_size, self.cate_size, self.campaign_id_size,
+            self.customer_size, self.brand_size, self.btag_size, self.pid_size,
+            self.main_embedding_size, self.other_embedding_size)
+
+        pred, loss = DMR_model(inputs, is_infer)
+
+        predict_2d = paddle.concat(x=[1 - pred, pred], axis=1)
+        auc, batch_auc, _ = paddle.static.auc(input=predict_2d,
+                                              label=label,
+                                              num_thresholds=2**12,
+                                              slide_steps=20)
+        auc = paddle.cast(auc, "float32")
+
+        if is_infer:
+            fetch_dict = {"auc": auc}
+            return fetch_dict
+
+        self._cost = loss
+        fetch_dict = {'auc': auc, 'cost': loss}
+        return fetch_dict
+
+    def create_optimizer(self, strategy=None):
+        optimizer = paddle.optimizer.Adam(
+            learning_rate=self.learning_rate, lazy_mode=False)
+        if strategy != None:
+            import paddle.distributed.fleet as fleet
+            optimizer = fleet.distributed_optimizer(optimizer, strategy)
+        optimizer.minimize(self._cost)
+
+    def infer_net(self, input):
+        return self.net(input, is_infer=True)
