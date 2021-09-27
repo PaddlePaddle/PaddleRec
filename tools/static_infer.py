a6dfad7759c8e9b6bb6094e8c4331fd8ba4919b9
yinhaofeng <1841837261@qq.com>
2021-09-27 12:52:06 +0000

    fix -o

--- tools/static_infer.py
+++ tools/static_infer.py
@@ -55,6 +55,8 @@ def main(args):
             key, value = parameter.split("=")
             if type(config.get(key)) is int:
                 value = int(value)
+            if type(config.get(key)) is float:
+                value = float(value)
             if type(config.get(key)) is bool:
                 value = (True if value.lower() == "true" else False)
             config[key] = value
