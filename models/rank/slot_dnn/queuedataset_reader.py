724b45b0ffba74cdaad1f2f6371f22e5d9c1c422
esythan <esythan@126.com>
2021-11-08 20:54:40 +0800

    change slot_num and layer_size

--- models/rank/slot_dnn/queuedataset_reader.py
+++ models/rank/slot_dnn/queuedataset_reader.py
@@ -32,7 +32,7 @@ class Reader(fleet.MultiSlotDataGenerator):
         self.config = config
         padding = 0
         #sparse_slots = "click 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26"
-        self.slots = self.slot_reader(slot_num=408)
+        self.slots = self.slot_reader(slot_num=300)
         self.slot2index = {}
         self.visit = {}
         for i in range(len(self.slots)):
