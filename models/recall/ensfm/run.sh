6b250e150ac4855bc33c53c71092e5468148ebb1
xly <imxly2@126.com>
2021-12-02 14:10:43 +0800

    Add ensfm

--- models/recall/ensfm/run.sh
+++ models/recall/ensfm/run.sh
@@ -12,6 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-echo "................run................."
+echo ".................run.................."
 python -u ../../../tools/trainer.py -m config_bigdata.yaml  &> log_train.txt
 python -u infer.py -m config.yaml &> result.txt
