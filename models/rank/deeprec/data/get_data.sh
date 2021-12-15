c170733724da182af50537f877cf9185f7a8f8a4
cHEn <1445366404@qq.com>
2021-12-15 10:34:45 +0800

    add DeepRec

---
+++ models/rank/deeprec/data/get_data.sh
@@ -0,0 +1,4 @@
+weget https://paddlerec.bj.bcebos.com/datasets/Netflix-DeepRec/nf_prize_dataset.tar
+tar -xvf nf_prize_dataset.tar.gz
+tar -xf download/training_set.tar
+python netflix_data_convert.py training_set Netflix
