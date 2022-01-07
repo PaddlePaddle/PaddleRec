ce99a8b919e495999ff6dcb65b20d701c5882f78
yinhaofeng <1841837261@qq.com>
2022-01-07 09:40:57 +0000

    fix bug

--- datasets/MIND/data/run.sh
+++ datasets/MIND/data/run.sh
@@ -6,13 +6,13 @@ unzip datasets%2FMIND%2FMINDlarge_train.zip  -d ./train_raw
 unzip datasets%2FMIND%2FMINDlarge_dev.zip  -d ./dev_raw
 unzip datasets%2FMIND%2FMINDlarge_test.zip  -d ./test_raw
 mkdir kkk
-python3 combine.py
-python3 preprocess.py --build_dict --build_dict_corpus_dir kkk --dict_path test_build_dict  
-python3 preprocess.py --filter_corpus --dict_path test_build_dict --input_corpus_dir kkk --output_corpus_dir convert_text8 --min_count 5 --downsample 0.001
+python combine.py
+python preprocess.py --build_dict --build_dict_corpus_dir kkk --dict_path test_build_dict  
+python preprocess.py --filter_corpus --dict_path test_build_dict --input_corpus_dir kkk --output_corpus_dir convert_text8 --min_count 5 --downsample 0.001
 mkdir train
 mkdir test
 mkdir dev
-python3 make_article.py
+python make_article.py
 cp article.txt train/
 cp article.txt dev/
 cp article.txt test/
@@ -22,4 +22,4 @@ mv cate_map dict/cate_map
 mv sub_cate_map dict/sub_cate_map
 mv test_build_dict_word_to_id_ dict/word_dict
 rm article.txt news_backup.tsv test_build_dict
-python3 generate_yaml.py
+python generate_yaml.py
