fix: add dygraph to static for sign

models/rank/sign/dygraph_model.py
@@ -40,7 +40,7 @@ class DygraphModel():
     # define model
     def create_model(self, config):
         pred_edges = config.get('hyper_parameters.pred_edges', 1)
-        dim = config.get('hyper_parameters.pred_edges', 8)
+        dim = config.get('hyper_parameters.dim', 8)
         hidden_layer = config.get('hyper_parameters.hidden_layer', 64)
         l0_para = config.get('hyper_parameters.l0_para', [0.66, -0.1, 1.1])
         batch_size = config.get('runner.train_batch_size', 8)
@@ -66,7 +66,12 @@ class DygraphModel():
             labels.append(batch_data[4][i].numpy())
         graphs = pgl.Graph.batch(graphs).tensor()
         labels = paddle.to_tensor(labels, dtype='float32')
-        return graphs, labels
+        edges = np.array(graphs.edges, dtype="int32")
+        node_feat = np.array(graphs.node_feat["node_attr"], dtype="int32")
+        edge_feat = np.array(graphs.edge_feat["edge_attr"], dtype="int32")
+        segment_ids = graphs.graph_node_id
+
+        return edges, node_feat, edge_feat, segment_ids, labels
 
     # define loss function by predicts and label
     def create_loss(self, output, label, l0_penaty, l2_penaty, l0_weight,
@@ -100,9 +105,11 @@ class DygraphModel():
 
     # construct train forward phase  
     def train_forward(self, dy_model, metrics_list, batch_data, config):
-        graphs, labels = self.create_feeds(batch_data, config)
+        edges, node_feat, edge_feat, segment_ids, labels = self.create_feeds(
+            batch_data, config)
         # predict
-        output, l0_penaty, l2_penaty = dy_model.forward(graphs, True)
+        output, l0_penaty, l2_penaty = dy_model.forward(
+            edges, node_feat, edge_feat, segment_ids, True)
         # get loss
         l0_weight = config.get("hyper_parameters.l0_weight", 0.001)
         l2_weight = config.get("hyper_parameters.l0_weight", 0.001)
@@ -122,9 +129,11 @@ class DygraphModel():
 
     # construct infer forward phase  
     def infer_forward(self, dy_model, metrics_list, batch_data, config):
-        graphs, labels = self.create_feeds(batch_data, config)
+        edges, node_feat, edge_feat, segment_ids, labels = self.create_feeds(
+            batch_data, config)
         # predict
-        output, _, _ = dy_model.forward(graphs, False)
+        output, _, _ = dy_model.forward(edges, node_feat, edge_feat,
+                                        segment_ids, False)
         # update metrics
         predictions = np.vstack(output)
         labels = np.vstack(labels)