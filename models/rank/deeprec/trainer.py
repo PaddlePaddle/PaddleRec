88c7dae972101a0cb37e634e93f32e55af44311c
wangzhen38 <wangzhen38@baidu.com>
2022-05-13 11:13:21 +0000

    fix deeprec&deselectk

--- models/rank/deeprec/trainer.py
+++ models/rank/deeprec/trainer.py
@@ -18,15 +18,15 @@ import time
 import logging
 import sys
 
-from tools.utils.utils_single import load_yaml, load_dy_model_class, \
-    get_abs_model, create_data_loader
-from tools.utils.save_load import load_model, save_model
-import argparse
-
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 print(os.path.abspath('/'.join(__dir__.split('/')[:-3])))
 sys.path.append(os.path.abspath(os.path.join(__dir__, '..')))
 sys.path.append(os.path.abspath('/'.join(__dir__.split('/')[:-3])))
+
+from tools.utils.utils_single import load_yaml, load_dy_model_class, \
+    get_abs_model, create_data_loader
+from tools.utils.save_load import load_model, save_model
+import argparse
 
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
