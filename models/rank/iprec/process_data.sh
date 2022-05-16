571bf19676f6c1a84bae901d921b275ebae6876a
xly <xgboost@qq.com>
2022-05-16 21:34:12 +0800

    Add iprec

--- models/rank/iprec/process_data.sh
+++
@@ -1,4 +0,0 @@
-python process_public_data.py
-mkdir data/whole_data && mkdir data/whole_data/train && mkdir data/whole_data/test
-mv data/ctr_cvr.train data/whole_data/train
-mv data/ctr_cvr.test data/whole_data/test
