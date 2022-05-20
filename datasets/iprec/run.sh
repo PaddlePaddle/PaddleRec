925a520ff573d4971905648cb752e883a3fed059
xly <xgboost@qq.com>
2022-05-20 18:30:12 +0800

    Fix

--- datasets/iprec/run.sh
+++ datasets/iprec/run.sh
@@ -1,5 +1,6 @@
 mkdir whole_data
 mkdir whole_data/train && mkdir whole_data/test
 wget https://paddlerec.bj.bcebos.com/datasets/IPREC/3_days.zip
+unzip 3_days.zip
 mv train.jsonl whole_data/train
 mv test.jsonl whole_data/test
