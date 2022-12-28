c807fec04b0aa8dc37082eabbd475bd17ce3a477
wangzhen38 <wangzhen38@baidu.com>
2022-12-28 07:39:48 +0000

    code style fix

--- recserving/milvus_tool/milvus_insert.py
+++ recserving/milvus_tool/milvus_insert.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from milvus import *
 
 # from milvus_tool.config import MILVUS_HOST, MILVUS_PORT, collection_param, index_type, index_param
@@ -26,7 +40,8 @@ class VecToMilvus():
 
     def create_index(self, collection_name):
         try:
-            status = self.client.create_index(collection_name, index_type, index_param)
+            status = self.client.create_index(collection_name, index_type,
+                                              index_param)
             print(status)
             return status
         except Exception as e:
@@ -34,14 +49,16 @@ class VecToMilvus():
 
     def has_partition(self, collection_name, partition_tag):
         try:
-            status, ok = self.client.has_partition(collection_name, partition_tag)
+            status, ok = self.client.has_partition(collection_name,
+                                                   partition_tag)
             return ok
         except Exception as e:
             print("Milvus has partition error: ", e)
 
     def create_partition(self, collection_name, partition_tag):
         try:
-            status = self.client.create_partition(collection_name, partition_tag)
+            status = self.client.create_partition(collection_name,
+                                                  partition_tag)
             print('create partition {} successfully'.format(partition_tag))
             return status
         except Exception as e:
@@ -52,13 +69,21 @@ class VecToMilvus():
             if not self.has_collection(collection_name):
                 self.creat_collection(collection_name)
                 self.create_index(collection_name)
-                print('collection info: {}'.format(self.client.get_collection_info(collection_name)[1]))
-            if (partition_tag is not None) and (not self.has_partition(collection_name, partition_tag)):
+                print('collection info: {}'.format(
+                    self.client.get_collection_info(collection_name)[1]))
+            if (partition_tag is not None) and (
+                    not self.has_partition(collection_name, partition_tag)):
                 self.create_partition(collection_name, partition_tag)
-            status, ids = self.client.insert(collection_name=collection_name, records=vectors, ids=ids,
-                                             partition_tag=partition_tag)
+            status, ids = self.client.insert(
+                collection_name=collection_name,
+                records=vectors,
+                ids=ids,
+                partition_tag=partition_tag)
             self.client.flush([collection_name])
-            print('Insert {} entities, there are {} entities after insert data.'.format(len(ids), self.client.count_entities(collection_name)[1]))
+            print(
+                'Insert {} entities, there are {} entities after insert data.'.
+                format(
+                    len(ids), self.client.count_entities(collection_name)[1]))
             return status, ids
         except Exception as e:
             print("Milvus insert error:", e)
@@ -72,6 +97,10 @@ if __name__ == '__main__':
     partition_tag = 'partition_1'
     ids = [random.randint(0, 1000) for _ in range(100)]
     embeddings = [[random.random() for _ in range(128)] for _ in range(100)]
-    status, ids = client.insert(collection_name=collection_name, vectors=embeddings, ids=ids,partition_tag=partition_tag)
+    status, ids = client.insert(
+        collection_name=collection_name,
+        vectors=embeddings,
+        ids=ids,
+        partition_tag=partition_tag)
     print(status)
     # print(ids)
--- recserving/movie_recommender/to_redis.py
+++ recserving/movie_recommender/to_redis.py
@@ -18,6 +18,7 @@ import redis
 import json
 import codecs
 
+
 #1::Toy Story (1995)::Animation|Children's|Comedy
 def process_movie(lines, redis_cli):
     for line in lines:
@@ -27,14 +28,13 @@ def process_movie(lines, redis_cli):
         movie_id = tmp[0]
         title = tmp[1]
         genre_group = tmp[2]
-        
+
         tmp = genre_group.strip().split("|")
         genre = tmp
-        movie_info = {"movie_id" : movie_id,
-                "title" : title,
-                "genre" : genre
-                }
-        redis_cli.set("{}##movie_info".format(movie_id), json.dumps(movie_info))
+        movie_info = {"movie_id": movie_id, "title": title, "genre": genre}
+        redis_cli.set("{}##movie_info".format(movie_id),
+                      json.dumps(movie_info))
+
 
 #1::F::1::10::48067
 def process_user(lines, redis_cli):
@@ -47,21 +47,22 @@ def process_user(lines, redis_cli):
         age = tmp[2]
         job = tmp[3]
         zip_code = tmp[4]
-        user_info = {"user_id": user_id,
-                "gender": gender,
-                "age": age,
-                "job": job,
-                "zip_code": zip_code
-                }
+        user_info = {
+            "user_id": user_id,
+            "gender": gender,
+            "age": age,
+            "job": job,
+            "zip_code": zip_code
+        }
         redis_cli.set("{}##user_info".format(user_id), json.dumps(user_info))
 
+
 if __name__ == "__main__":
-    r = redis.StrictRedis(host="127.0.0.1", port="6379") 
-    with codecs.open("users.dat", "r",encoding='utf-8',errors='ignore') as f:
+    r = redis.StrictRedis(host="127.0.0.1", port="6379")
+    with codecs.open("users.dat", "r", encoding='utf-8', errors='ignore') as f:
         lines = f.readlines()
         process_user(lines, r)
-    with codecs.open("movies.dat", "r",encoding='utf-8',errors='ignore') as f:
+    with codecs.open(
+            "movies.dat", "r", encoding='utf-8', errors='ignore') as f:
         lines = f.readlines()
         process_movie(lines, r)
-
-    
