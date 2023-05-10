5f572880f1fa40ae586a358c64242436f8beb651
wangzhen38 <41941775+wangzhen38@users.noreply.github.com>
2023-05-10 09:35:55 +0800

    [bug fix] from paddle udpate (#922)
    * [bug fix] from paddle udpate
    
    * [bug fix] logger info

--- tools/static_trainer.py
+++ tools/static_trainer.py
@@ -31,6 +31,7 @@ import argparse
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def parse_args():
